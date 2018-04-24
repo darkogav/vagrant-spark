b88e70033af6b3fb01d47fef0dd81ef94c97240c
Darko Gavrilovic <darko@chass.utoronto.ca>
2018-04-24 12:49:37 -0400

    update files

--- vagrant-master/setup.sh
+++ vagrant-master/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Setting up vagrant image for Spark master"
+echo "Setting up vagrant image for Spark Master"
 echo -e "192.168.50.11\tspark-master-priv\n192.168.50.12\tspark-slave-priv\n" >> /etc/hosts
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm >>/tmp/setup.log 2>&1
 yum-config-manager --enable epel -y >>/tmp/setup.log 2>&1
@@ -18,6 +18,10 @@ tar xvf /opt/spark-2.3.0-bin-hadoop2.7.tgz -C /opt >>/tmp/setup.log 2>&1
 #chcon -t httpd_sys_content_t /opt/spark -R >>/tmp/provision-script.log 2>&1
 chown -R root:root /opt >>/tmp/setup.log 2>&1
 ln -s /opt/spark-2.3.0-bin-hadoop2.7 /opt/spark >>/tmp/setup.log 2>&1
+cp /home/vagrant/hosts /etc/hosts
+cp /home/vagrant/spark.service /etc/systemd/system/spark.service
+systemctl enable spark
+systemctl start spark
 pip install --upgrade pip
 pip install --upgrade pyspark
 
